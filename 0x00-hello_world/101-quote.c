e0c49399fb711a0d164d5058720e6645dcc5a923
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-07 07:17:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
