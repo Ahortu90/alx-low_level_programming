698efcb7cf8a70b32ebc80633f6b70ba2019c349
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-07 12:29:23 +0000

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Prinys quots to the standard error
+ * main - Prints quote to the standard error
  *
  * Return: zero on success
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+        write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+        return (1);
 }
