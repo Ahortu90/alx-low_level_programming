5f2fc34ba43e04bc6017c248ede385065e57289c
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-12 15:11:25 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-	char word[9] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
-	for (i = 0; i < 9; i++)
+	
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
