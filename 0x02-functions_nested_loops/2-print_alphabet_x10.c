a407f324f53f4db6bb6bd5e79a3f09a016d35e9c
emaalor <emmanuelalor@gmail.com>
2022-04-12 10:06:52 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "2-print_alphabet_x10.h"
+
+/**
+ * print_alphabet_x10 - Check 2-print_alphabet_x10
+ * Description: function uses _putchar function to print
+ alphabet in lowercases 10 time
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
