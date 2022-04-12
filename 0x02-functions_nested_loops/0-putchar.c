de255f6e760e509736a9c2a8c94a80c43ab9f77e
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-12 14:59:57 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-        char word[9] = "Holberton";
-        int i;
+	char c[] = "_putchar";
+	int i;
 
-        for (i = 0; i < 9; i++)
-                _putchar(word[i]);
-        _putchar('\n');
-
-        return (0);
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
-
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,29 @@
 #include "main.h"
-
 /**
- * print_sign - Check 5-sign
- * @n: An input number
- * Description: Tohis function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
+ * print_sign - Determines whether an integer is positive, negative or zero.
+ * @n: the number to be checked.
+ * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
-int print_sing(int n)
+int print_sign(int n)
 {
-	int value;
-
 	if (n > 0)
 	{
-		value = 1;
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		value = 0;
 		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		value = -1;
 		_putchar('-');
+		return ('/');
 	}
-
-	return (value);
 }
