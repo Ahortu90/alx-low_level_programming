624897ecf7f4cc33d9566e6875d67d7a97abc497
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-14 09:14:49 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ *
+ * Return: Always 0 (Success).
+ */
+void print_number(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
