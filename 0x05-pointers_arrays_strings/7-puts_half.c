6acdf598cd37c2324bd645fe594a003f22d3c7df
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-20 12:14:12 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *puts_half - print half string
+ *@str: string
+ *
+ *Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			if (j >= i / 2)
+				_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			if (j > i / 2)
+				_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
+"7-puts_half.c" 37L, 391C
