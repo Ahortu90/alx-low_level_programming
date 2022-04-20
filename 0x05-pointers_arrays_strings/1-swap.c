e3de5fea7675245772a87d82ab8a00c76fe4cb65
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-20 11:33:17 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#iclude "main"
+
+/**
+ * swap_int - swap numbers function
+ * @a: integer
+ * @b: integer
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _strlen - count the length of a string
+ * @s: string
+ *
+ * Return: the Value of the lenght i
+ */
+
+int -strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
