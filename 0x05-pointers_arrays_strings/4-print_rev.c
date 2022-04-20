77fe0fec644243a5e0a5431ab886d237b97d76cc
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-20 12:39:09 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: characters
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	while (i)
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}
