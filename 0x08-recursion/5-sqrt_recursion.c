d57920c9427f4ff75fe724ff224cd4d3ddc55bba
Ahortu90 <ahortuderrick4@gmail.com>
2022-04-28 14:29:01 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *square - square conditions
+ *@i: integer
+ *@n: integer
+ *
+ *Return: square of number
+ */
+
+int square(int i, int n)
+{
+	if (i * i == n)
+		return (i);
+	else if (i > n / 2)
+		return (-1);
+	else
+		return (square(i + 1, n));
+}
+
+/**
+ *_sqrt_recursion - square integer
+ *@n: integer
+ *
+ *Return: square of number
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(1, n));
+}
