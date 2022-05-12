eab47b23063b5e4947fbcc11c55df090dc23fd81
Ahortu90 <ahortuderrick4@gmail.com>
2022-05-12 23:19:23 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,38 +2,6 @@
 #include <stdlib.h>
 
 /**
- *_strdup - duplicate a string
- *@str: string
- *
- *Return: pointer to newely allocated space in memory
- */
-char *_strdup(char *str)
-{
-	char *str1;
-	int i, j;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i]; i++)
-	{
-		;
-	}
-	i++;
-	str1 = malloc(sizeof(char) * i);
-
-	if (str1 == NULL)
-	{
-		return (NULL);
-	}
-	for (j = 0; j < i; j++)
-		str1[j] = str[j];
-
-	return (str1);
-}
-
-/**
  *new_dog - structure
  *@name: array of character
  *@age: floate
